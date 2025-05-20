263ed68708bb0b076abaf43302bb6eda9971d3f4
FacuBustamaante <facundombustamante@gmail.com>
2025-05-20 10:04:04 -0300

    Clase base actualizada

--- src/Entities/Base.java
+++ src/Entities/Base.java
@@ -1,4 +1,15 @@
 package Entities;
 
 public class Base {
+    private Long id;
+
+    public Base(){
+    }
+    public Base(Long id){
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {}
 }
